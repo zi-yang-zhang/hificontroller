17cda4fdee19d3cff7352f0edebc6b7aa32db0f9
robzzyzhang <robzzyzhang@tencent.com>
2022-09-14 00:51:07 +0800

    fix

--- device.py
+++ device.py
@@ -1,6 +1,6 @@
 
 from driver import config
-
+from hifiLogger import logger
 # 树莓派的设备抽象
 # 这里通信是走rs232协议
 
@@ -9,11 +9,14 @@ class Device:
         self.__channel__ = config.config(Baudrate=baudrate, dev = "/dev/ttySC{}".format(channelNumber - 1))
 
     def send(self, request, needResponse = False, parser = None, bufferSize = 26):
+        logger.debug("Send request %s", request)
         self.__channel__.Uart_SendString(request)
         if needResponse: 
+            response = self.__channel__.Uart_ReceiveString(bufferSize)
+            logger.debug("Response:%s",response)
             if parser is None:
-                return self.__channel__.Uart_ReceiveString(bufferSize)
-            return parser(self.__channel__.Uart_ReceiveString(bufferSize))
+                return response
+            return parser(response)
         return "No response"
         
     def getChannel(self):
