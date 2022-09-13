5d0f2f807a34907ba119e2a78c767e5c26eebb59
robzzyzhang <robzzyzhang@tencent.com>
2022-09-14 01:04:25 +0800

    fix

--- hifiLogger.py
+++ hifiLogger.py
@@ -1,8 +1,13 @@
 import logging
+import sys
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
 logging.basicConfig(filename='./log/server.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
+                    handlers=[handler],
                     level=logging.DEBUG)
 
 logger = logging.getLogger('hifiController')
