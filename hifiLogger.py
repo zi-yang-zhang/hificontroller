c437b53ad2e31682fe2eccf547307f31ef15f67d
robzzyzhang <robzzyzhang@tencent.com>
2022-09-14 01:05:33 +0800

    fix log

--- hifiLogger.py
+++ hifiLogger.py
@@ -7,7 +7,6 @@ logging.basicConfig(filename='./log/server.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
-                    handlers=[handler],
                     level=logging.DEBUG)
 
 logger = logging.getLogger('hifiController')
