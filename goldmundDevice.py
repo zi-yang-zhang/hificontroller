fa501f1762cce8cdd0bdb0ebfa2ac09832e038ea
robzzyzhang <robzzyzhang@tencent.com>
2022-09-17 02:25:33 +0800

    fix log

--- goldmundDevice.py
+++ goldmundDevice.py
@@ -21,59 +21,59 @@ class GoldmundStrategy:
     
     # 当主设备开启时，先记录当前设备状态，然后再调整到AV配置，如果没开机的话先开机
     def onMainDevicePowerOn(self):
-        logger.info("Main device is on, try setInput to {} and setVolume to {}", AV_INPUT, AV_VOLUME)
+        logger.info("Main device is on, try setInput to %s and setVolume to %s", AV_INPUT, AV_VOLUME)
         if self.__standby:
             logger.info("Goldmund is off, turining it on")
             code, result = self.send(standby(False))
-            logger.debug("Standby result: {}", code)
+            logger.debug("Standby result: %s", code)
             if not isSuccess(code):
-                logger.error("Standby command failed {},{}", code, result)
+                logger.error("Standby command failed %s,%s", code, result)
                 return
         if self.__input != AV_INPUT:
             code, result = self.send(setInput(AV_INPUT))
-            logger.debug("Set Input result: {}", code)
+            logger.debug("Set Input result: %s", code)
         if self.__volume != AV_VOLUME:
             code, result = self.send(setVolume(AV_VOLUME))
-            logger.debug("Set Volume result: {}", code)
+            logger.debug("Set Volume result: %s", code)
         pass
     
     # 回复上一次状态，如果上一次的状态本来就跟AV状态一样则不变。如果上一次是standby的则会关机
     def restoreStatus(self):
-        logger.info("Restoring status for goldmund: standby {}, input {}, volume {}", str(self.__standby), self.__input, self.__volume)
+        logger.info("Restoring status for goldmund: standby %s, input %s, volume %s", self.__standby, self.__input, self.__volume)
         # 本来是关的就关掉
         if self.__standby:
             code, result = self.send(standby(self.__standby))
-            logger.debug("Set standby result: {}", code)
+            logger.debug("Set standby result: %s", code)
             if isSuccess(code):
                 return
         if self.__volume != AV_VOLUME:
             code, result = self.send(setVolume(self.__volume))
-            logger.debug("Set Volume result: {}", code)
+            logger.debug("Set Volume result: %s", code)
         if self.__input != AV_INPUT:
             code, result = self.send(setInput(self.__input))
-            logger.debug("Set Input result: {}", code)
+            logger.debug("Set Input result: %s", code)
     
     # 查看机器状态，将开关机/输入/音量记录下来
     def checkAndSetStatus(self):
         code, standbyStatus = self.send(query(standby_command))
         if not isSuccess(code):
-            logger.error("Query failed {},{}", code, standbyStatus)
+            logger.error("Query failed %s,%s", code, standbyStatus)
             return
         command, status = standbyStatus.split(" ")
         code, input = self.send(query(input_command))
         if not isSuccess(code):
-            logger.error("Query failed {},{}", code, input)
+            logger.error("Query failed %s,%s", code, input)
             return
         command, inputNumber = input.split(" ")
         code, volume = self.send(query(volume_command))
         if not isSuccess(code):
-            logger.error("Query failed {},{}", code, volume)
+            logger.error("Query failed %s,%s", code, volume)
             return
         command, volumeNumber = volume.split(" ")
         self.__input = inputNumber
         self.__standby = status == "on"
         self.__volume = volumeNumber
-        logger.info("Current status for goldmund: standby {}, input {}, volume {}", str(self.__standby), self.__input, self.__volume)
+        logger.info("Current status for goldmund: standby %s, input %s, volume %s", self.__standby, self.__input, self.__volume)
         return 
 
 
