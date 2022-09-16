78640f959a3d968c12a0a916635c663f1f44efd8
robzzyzhang <robzzyzhang@tencent.com>
2022-09-16 22:15:55 +0800

    fix

--- goldmundDevice.py
+++ goldmundDevice.py
@@ -3,7 +3,7 @@ from hifiController import Peripheral
 from device import Device
 from hifiLogger import logger
 # Goldmund设备策略
-# 主设备开时开设备，并调节到频道2，音量60
+# 主设备开时开设备，并调节到频道7，音量85
 
 
 class GoldmundStrategy:
@@ -19,7 +19,7 @@ class GoldmundStrategy:
         pass
 
     def onMainDevicePowerOn(self):
-        logger.info("Main device is on, try set input to 2 and volumn to 60")
+        logger.info("Main device is on, try set input to 7 and volumn to 70")
         result = self.__device__.send(
             query(standby_command), True, parseResponse)
         if result[1] == "off":
@@ -28,8 +28,8 @@ class GoldmundStrategy:
         else:
             self.wasOn = True
         logger.info("Device was on %s", self.wasOn)
-        logger.debug(self.__device__.send(setInput(2)))
-        logger.debug(self.__device__.send(setVolumn(60)))
+        logger.debug(self.__device__.send(setInput(7)))
+        logger.debug(self.__device__.send(setVolumn(70)))
         pass
 
 
